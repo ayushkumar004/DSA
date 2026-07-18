27ed002da5004917764dab6801549c5dfba7e68c
ayushkumar004 <153125113+ayushkumar004@users.noreply.github.com>
2026-07-18 09:55:51 +0530

    Leetcode:169And905

---
+++ 1array/169LeetCode.cpp
@@ -0,0 +1,10 @@
+//169. Majority Element
+
+//1st method: Sorting
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+    }
+};
